01c80fc9963bfe323171b262b0b5371f067a5ee4
Даниил <shabanovdan11l@mail.ru>
2025-03-26 16:41:20 +0300

    fix

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv('DEBUG', 1))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'barbershop-v42x.onrender.com']
+# Разрешенные хосты
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1,barbershop-v42x.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
